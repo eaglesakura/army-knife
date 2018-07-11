a412808e2f4d49f908d34983ce8eb23435203a05
eaglesakura <eagle.sakura@gmail.com>
2018-07-11 18:07:58 +0900

    wip

--- .circleci/scripts/setup.sh
+++ .circleci/scripts/setup.sh
@@ -4,7 +4,6 @@ echo "#######################################"
 echo "# checkout secret files"
 echo "#######################################"
 mkdir ${CIRCLE_ARTIFACTS}
-chmod +x -R ./scripts
 chmod +x ./gradlew
 
 ./gradlew --parallel androidDependencies
