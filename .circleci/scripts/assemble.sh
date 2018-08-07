9be8027e1dd732355bee2458f58f02381df3a96f
eaglesakura <eagle.sakura@gmail.com>
2018-08-07 17:15:11 +0900

    wip: faster assemble

--- .circleci/scripts/assemble.sh
+++ .circleci/scripts/assemble.sh
@@ -4,6 +4,4 @@ echo "#######################################"
 echo "# assemble"
 echo "#######################################"
 ./gradlew --parallel assembleAndroidTest
-./gradlew --parallel assembleDebug
-./gradlew --parallel assembleRelease
 
