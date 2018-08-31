bb43282d216ed756ba235210e3cce94faca2de42
eaglesakura <eagle.sakura@gmail.com>
2018-08-31 16:22:10 +0900

    add: Added string-validator methods

--- army-knife-junit/src/main/java/com/eaglesakura/armyknife/junit/StringValidator.kt
+++ army-knife-junit/src/main/java/com/eaglesakura/armyknife/junit/StringValidator.kt
@@ -13,4 +13,12 @@ class StringValidator(var actual: String) {
     fun notContains(expected: CharSequence) {
         assertTrue("not contains | expected \"%s\" | actual \"%s\"".format(expected, actual), !actual.contains(expected))
     }
+
+    fun startsWith(expected: CharSequence) {
+        assertTrue("not startsWith | expected \"%s\" | actual \"%s\"".format(expected, actual), actual.startsWith(expected))
+    }
+
+    fun endsWith(expected: CharSequence) {
+        assertTrue("not endsWith | expected \"%s\" | actual \"%s\"".format(expected, actual), actual.endsWith(expected))
+    }
 }
