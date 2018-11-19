03f0504dc0b55dd1868dd4eb97c225e9da247963
eaglesakura <eagle.sakura@gmail.com>
2018-11-19 13:02:21 +0900

    add: added Runtime flags refs #19

--- army-knife/src/main/java/com/eaglesakura/armyknife/android/ApplicationRuntime.kt
+++ army-knife/src/main/java/com/eaglesakura/armyknife/android/ApplicationRuntime.kt
@@ -14,6 +14,7 @@ import android.content.pm.ResolveInfo
 import android.os.Build
 import android.view.Surface
 import android.view.WindowManager
+import androidx.annotation.IntDef
 
 /**
  * Utility for Application Runtime information.
@@ -63,6 +64,87 @@ object ApplicationRuntime {
         true
     } catch (err: ClassNotFoundException) {
         false
+    }
+
+
+    /**
+     * Run on JUnit test case.
+     */
+    const val RUNTIME_JUNIT = 0x01 shl 0
+
+    /**
+     * Run on JUnit with Robolectric.
+     *
+     * @link http://robolectric.org/
+     */
+    const val RUNTIME_ROBOLECTRIC = 0x01 shl 1
+
+    /**
+     * Run on JUnit with Instrumentation Test in Android device.
+     */
+    const val RUNTIME_INSTRUMENTATION = 0x01 shl 2
+
+    /**
+     * Run on Android device.
+     */
+    const val RUNTIME_ANDROID_DEVICE = 0x01 shl 3
+
+    /**
+     * Run on ART VM in Android device.
+     */
+    const val RUNTIME_ART = 0x01 shl 4
+
+    /**
+     * Run on Dalvik VM in Android device.
+     */
+    const val RUNTIME_DALVIK = 0x01 shl 5
+
+    /**
+     * Run on Java VM on PC.
+     */
+    const val RUNTIME_JAVA_VM = 0x01 shl 6
+
+    /**
+     * Runtime flags in current Virtual Machine(ART in Android or JVM in PC, or such else)
+     *
+     * @see RUNTIME_JUNIT
+     * @see RUNTIME_ROBOLECTRIC
+     * @see RUNTIME_INSTRUMENTATION
+     * @see RUNTIME_ANDROID_DEVICE
+     * @see RUNTIME_ART
+     * @see RUNTIME_DALVIK
+     * @see RUNTIME_JAVA_VM
+     */
+    val runtimeFlags: Int = getRuntimeFlags()
+
+    @Retention(AnnotationRetention.BINARY)
+    @IntDef(
+        RUNTIME_JUNIT,
+        RUNTIME_ROBOLECTRIC,
+        RUNTIME_INSTRUMENTATION,
+        RUNTIME_ANDROID_DEVICE,
+        RUNTIME_ART,
+        RUNTIME_DALVIK,
+        RUNTIME_JAVA_VM
+    )
+    annotation class RuntimeFlag
+
+    /**
+     * Check runtime has all flags.
+     *
+     * e.g.)
+     * if(ApplicationRuntime.runIn(RUNTIME_ART, RUNTIME_INSTRUMENTATION)) {
+     *      // ART with Instrumentation test.
+     * }
+     */
+    fun runIn(
+        @RuntimeFlag flag: Int, @RuntimeFlag vararg flags: Int
+    ): Boolean {
+        var allFlags = flag
+        flags.forEach { flg ->
+            allFlags = allFlags or flg
+        }
+        return (runtimeFlags and allFlags) == allFlags
     }
 
     /**
@@ -187,5 +269,35 @@ object ApplicationRuntime {
 
         return false
     }
+}
 
+private fun getRuntimeFlags(): Int {
+    // check Virtual Machine.
+    val virtualMachineFlags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        ApplicationRuntime.RUNTIME_ART
+    } else {
+        ApplicationRuntime.RUNTIME_DALVIK
+    }
+
+    try {
+        Class.forName("org.junit.Assert")
+    } catch (e: ClassNotFoundException) {
+        return virtualMachineFlags or ApplicationRuntime.RUNTIME_ANDROID_DEVICE
+    }
+
+    val runtimeName = System.getProperty("java.runtime.name")?.toLowerCase() ?: ""
+    return when {
+        runtimeName.isEmpty() -> {
+            // Android Runtime.
+            virtualMachineFlags or ApplicationRuntime.RUNTIME_ANDROID_DEVICE
+        }
+        runtimeName.contains("android") -> {
+            // in Instrumentation test.
+            virtualMachineFlags or ApplicationRuntime.RUNTIME_ANDROID_DEVICE or ApplicationRuntime.RUNTIME_JUNIT or ApplicationRuntime.RUNTIME_INSTRUMENTATION
+        }
+        else -> {
+            // Only Java VM
+            ApplicationRuntime.RUNTIME_JUNIT or ApplicationRuntime.RUNTIME_ROBOLECTRIC or ApplicationRuntime.RUNTIME_JAVA_VM
+        }
+    }
 }
