ca8a511e253c327525db7e55c05745c845736f18
eaglesakura <eagle.sakura@gmail.com>
2018-11-22 13:06:41 +0900

    add: added InputStream.readBytesInCoroutines()

--- army-knife/src/main/java/com/eaglesakura/armyknife/runtime/extensions/StreamExtensions.kt
+++ army-knife/src/main/java/com/eaglesakura/armyknife/runtime/extensions/StreamExtensions.kt
@@ -1,9 +1,34 @@
 package com.eaglesakura.armyknife.runtime.extensions
 
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.yield
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.InputStream
+
+/**
+ * Read all data from InputStream.
+ * This method supported for Cancel in read process.
+ *
+ * @author @eaglesakura
+ * @link https://github.com/eaglesakura/army-knife
+ */
+@Throws(IOException::class)
+suspend fun InputStream.readBytesInCoroutines(readOnce: Int = 1024): ByteArray {
+    val out = ByteArrayOutputStream(readOnce)
+    val buffer = ByteArray(readOnce)
+
+    do {
+        val bytes = read(buffer)
+        if (bytes < 0) {
+            yield()
+            return out.toByteArray()
+        } else {
+            yield()
+            out.write(buffer, 0, bytes)
+        }
+    } while (true)
+}
 
 /**
  * Read all data from InputStream.
